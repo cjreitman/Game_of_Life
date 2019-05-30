f7157769d64c950514c399946a360028840d8661
Colin <colin.reitman87@gmail.com>
2019-05-30 11:47:07 -0400

    final styling

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import './App.css';
 function App() {
   return (
     <div>
-      
-      <div className="app">
-        <h1 className="game-header">Conway's Game of Life</h1>
-        <Cosmos/>
-      </div>
+      <h1 className="game-header">Conway's Game of Life</h1>
+        <div className="app">
+          <Cosmos/>
+        </div>
+      <p className="instructions" > The Game of Life is a cellular automaton </p>
+      <p className="instructions1" > Cells live and die based on how many neighbors they have </p>
+      <p className="instructions2" > Click individual cells or Populate Cosmos to design a game </p>
+      <p className="instructions2" > Dare to Dream to see the Game in action </p>
     </div>
   );
 }
